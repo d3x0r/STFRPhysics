0d888750cfe09c54d96bd65ed6b2c3cac4b00ab0
d3x0r <d3x0r@no-reply.github.com>
2021-05-05 18:32:53 -0700

    Update personal extensions

--- 3d/src/three.js/personalFill.mjs
+++ 3d/src/three.js/personalFill.mjs
@@ -1,10 +1,6 @@
 
 import * as THREE from "./three.module.js"
 import {lnQuat} from "../lnQuatSq.js"
-if( !THREE.REVISION.includes( "d3x0r" ) ) {
-}
-if( Number(THREE.REVISION) === 74 ) {
-}
 
 var vector3Pool = [];
 export const Vector3Pool = {
@@ -95,6 +91,10 @@ export class Motion {
 	orientation = new lnQuat();
 	rotation = new lnQuat();
 	torque = new lnQuat();
+	eTorque = new lnQuat();
+
+	dipole = new lnQuat();
+	dipoleVec = new THREE.Vector3();
 	mass = 1.0;
 
 
@@ -102,6 +102,48 @@ export class Motion {
 		this.body = body;
 	}
 
+
+	affect( motion, inverse, delta ) {
+		const tmp1 = new lnQuat();
+		const tmp2 = new lnQuat();
+		this.dipoleVec.x = this.dipole.x;
+		this.dipoleVec.y = this.dipole.y;
+		this.dipoleVec.z = this.dipole.z;
+		const relPole = this.orientation.update().apply( this.dipoleVec );  
+		motion.dipoleVec.x = motion.dipole.x;
+		motion.dipoleVec.y = motion.dipole.y;
+		motion.dipoleVec.z = motion.dipole.z;
+		const otherPole = motion.orientation.update().apply( motion.dipoleVec );
+		tmp1.x = relPole.x;
+		tmp1.y = relPole.y;
+		tmp1.z = relPole.z;
+		tmp1.dirty = true;
+		tmp2.x = otherPole.x;
+		tmp2.y = otherPole.y;
+		tmp2.z = otherPole.z;
+		tmp2.dirty = true;
+		tmp1.update(); tmp2.update();
+
+		//this.dipole.update();
+		//motion.dipole.update();
+		const torque = new lnQuat();// 0, tmp2.x-tmp1.x,tmp2.y-tmp1.y,tmp2.z-tmp1.z);
+		if(  false && inverse )
+			tmp2.cross( tmp1, torque );
+		else
+			tmp1.cross( tmp2, torque );
+		const bodyDel = Vector3Pool.new().subVectors(  this.body.position, motion.body.position );
+		const rSq = bodyDel.lengthSq()/100;
+		bodyDel.delete();
+		this.eTorque.add( torque, 1/rSq );
+		//this.rotation.add( torque, delta/rSq );
+		//this.acceleration.add( bodyDel )
+	}
+	
+	start() {
+		this.eTorque.set( 0, 0, 0, 0 );
+		this.torque.set( 0, 0, 0, 0 );
+		this.acceleration.set( 0, 0, 0 );
+	}
                 move ( m, delta ) {
 
 					//this.orientation.spin( this.rotation.θ ,this.rotation.freeSpin( this.torque.θ * delta, this.torque ), delta ).exp( this.body.quaternion, 1 );
@@ -109,11 +151,13 @@ export class Motion {
 					if( this.torque.θ) {
 						//console.log( "Updating rotation:", this.rotation, this.torque )
 						tmpQ.set( this.torque ).freeSpin( -this.orientation.θ, this.orientation );
+						tmpQ.add( this.eTorque )
 						this.rotation.freeSpin( tmpQ.θ * delta, tmpQ );
 					}
 					this.rotation.update();
-					this.orientation.spin( this.rotation.θ * delta, {x:this.rotation.nx,
-							y:this.rotation.ny, z:this.rotation.nz } ).exp( this.body.quaternion, 1 );
+					this.orientation.spin( this.rotation.θ * delta, {x:this.rotation.nx
+							, y:this.rotation.ny
+							, z:this.rotation.nz } ).exp( this.body.quaternion, 1 );
 
 					this.speed.addScaledVector( this.acceleration, delta );
 					var del = this.speed.clone().multiplyScalar( delta );
@@ -131,22 +175,46 @@ export class Motion {
                 freemove( m, delta ) {
 					
 					var del = this.acceleration.clone().multiplyScalar( delta );
-					this.speed.addScaledVector( m.forward, del.z );
-					this.speed.addScaledVector( m.up, del.y );
-					this.speed.addScaledVector( m.left, del.x );
+					const basis = this.orientation.getBasis();
+					this.speed.addScaledVector( basis.forward, del.z );
+					this.speed.addScaledVector( basis.up, del.y );
+					this.speed.addScaledVector( basis.right, -del.x );
+
 					del.delete();
 
 					this.body.position.addScaledVector( this.speed, delta );
 
+					//const tq = {x:this.torque.x, y:this.torque.y, z:this.torque.z}
+					//const gtorque = tq;//this.orientation.applyDel( tq );
+
+					//tmpQ.set( 0, gtorque.x, gtorque.y, gtorque.z );
+					//tmpQ.add( this.eTorque )
 					this.torque.update();
 					if( this.torque.θ) {
 						//console.log( "Updating rotation:", this.rotation, this.torque )
 						///tmpQ.set( this.torque ).freeSpin( this.orientation.θ, this.orientation );
 						//this.rotation.freeSpin( tmpQ.θ * delta, tmpQ );
-						this.rotation.freeSpin( this.torque.θ * delta, this.torque ).update();
+						this.rotation.spin( this.torque.θ * delta, this.torque );
 					}
-					this.orientation.spin( this.rotation.θ * delta, {x:this.rotation.nx,
-							y:this.rotation.ny, z:this.rotation.nz } ).exp( this.body.quaternion, 1 );
+					this.eTorque.update();
+					tmpQ.set( 0, this.eTorque.x + this.rotation.x, this.eTorque.y + this.rotation.y, this.eTorque.z + this.rotation.z );
+					tmpQ.update();
+					//this.rotation.spin( this.eTorque.θ * delta, this.eTorque ).update();
+					//this.rotation.add( this.eTorque,  delta ).update();
+					
+					this.orientation.spin( this.rotation.θ * delta, {x:this.rotation.nx
+							, y:this.rotation.ny
+							, z:this.rotation.nz } ).exp( this.body.quaternion, 1 );
+					
+					this.orientation.spin( this.eTorque.θ * delta, {x:this.eTorque.nx
+							, y:this.eTorque.ny
+							, z:this.eTorque.nz } ).exp( this.body.quaternion, 1 );
+					if(0)
+					this.orientation.spin( tmpQ.θ * delta, {x:tmpQ.nx
+						, y:tmpQ.ny
+						, z:tmpQ.nz } ).exp( this.body.quaternion, 1 );
+	
+								
 					//this.orientation.add( this.rotation.freeSpin( this.torque.θ * delta, this.torque ), delta ).exp( this.body.rotation, 1 );
 				}
 				rotate( m, delta ) {
@@ -222,107 +290,3 @@ export class Motion {
 
 }
 
-THREE.Matrix4.prototype.__defineGetter__( "origin", function(){
-	if( !this._origin ){
-		var self = this;
-		this._origin = new THREE.Vector3();
-		Object.defineProperty(this, "_origin", { writable:false } );
-		Object.defineProperty( this._origin, "x", {
-			get : function(){
-				return self.elements[12];
-			},
-			set : function(v){
-				self.elements[12] = v;
-			}
-		})
-		Object.defineProperty( this._origin, "y", {
-			get : ()=>{
-				return self.elements[13];
-			},
-			set : (v)=>{
-				self.elements[13] = v;
-			}
-		})
-		Object.defineProperty( this._origin, "z", {
-			get : ()=>{
-				return self.elements[14];
-			},
-			set : (v)=>{
-				self.elements[14] = v;
-			}
-		})
-	}
-	return this._origin;
-} );
-
-
-
-
-	THREE.Matrix4.prototype.Translate = function(x,y,z) {
-			this.origin.x = x;
-			this.origin.y = y;
-			this.origin.z = z;
-	};
-	THREE.Matrix4.prototype.__defineGetter__( "left", function(){
-        	return Vector3Pool.new( this.elements[0], this.elements[1], this.elements[2] );
-        } );
-	THREE.Matrix4.prototype.__defineGetter__( "right", function(){
-		return Vector3Pool.new( -this.elements[0], -this.elements[1], -this.elements[2] );
-	} );
-	THREE.Matrix4.prototype.__defineGetter__( "up", function(){
-        	return Vector3Pool.new( this.elements[4], this.elements[5], this.elements[6] );
-        } );
-	THREE.Matrix4.prototype.__defineGetter__( "down", function(){
-		return Vector3Pool.new( -this.elements[4], -this.elements[5], -this.elements[6] );
-        } );
-	THREE.Matrix4.prototype.__defineGetter__( "forward", function(){
-        	return Vector3Pool.new( -this.elements[8], -this.elements[9], -this.elements[10] );
-        } );
-	THREE.Matrix4.prototype.__defineGetter__( "backward", function(){
-		return Vector3Pool.new( this.elements[8], this.elements[9], this.elements[10] );
-        } );
-	THREE.Matrix4.prototype.move = function (tick) {
-        	if( this.motion )
-				this.motion.move( this, tick );
-        	//this.origin.addScaledVector( this.forward, z ).addScaledVector( this.up, y ).addScaledVector( this.left, x )
-		};
-	THREE.Matrix4.prototype.moveNow = function ( x,y,z ) { this.origin.addScaledVector( this.forward, z ).addScaledVector( this.up, y ).addScaledVector( this.left, x ) };
-	THREE.Matrix4.prototype.moveForward = function ( n ) { this.origin.addScaledVector( this.forward, n ); };
-	THREE.Matrix4.prototype.moveUp = function ( n ) { this.origin.addScaledVector( this.up, n ); };
-	THREE.Matrix4.prototype.moveLeft = function ( n ) { this.origin.addScaledVector( this.left, n ); };
-	THREE.Matrix4.prototype.moveBackward = function ( n ) { this.origin.addScaledVector( this.backward, n ); };
-	THREE.Matrix4.prototype.moveDown = function ( n ) { this.origin.addScaledVector( this.down, n ); };
-	THREE.Matrix4.prototype.moveRight = function ( n ) { this.origin.addScaledVector( this.right, n ); };
-
-	THREE.Matrix4.prototype.__defineGetter__( "inv_left", function(){
-        	return Vector3Pool.new( this.elements[0], this.elements[4], this.elements[8] );
-        } );
-	THREE.Matrix4.prototype.__defineGetter__( "inv_up", function(){
-        	return Vector3Pool.new( this.elements[1], this.elements[5], this.elements[9] );
-        } );
-	THREE.Matrix4.prototype.__defineGetter__( "inv_forward", function(){
-        	return Vector3Pool.new( this.elements[2], this.elements[6], this.elements[10] );
-        } );
-
-	THREE.Matrix4.prototype.getRoll = function( relativeUp ) {
-		//if( !relativeUp ) relativeUp = THREE.Vector3Up;
-		return Math.asin( this.right.dot( relativeUp ) );
-	};
-	THREE.Matrix4.prototype.getPitch = function( relativeForward ) {
-		//if( !relativeForward ) relativeForward = THREE.Vector3Forward;
-		return Math.asin( this.up.dot( relativeForward ) );
-	};
-	THREE.Matrix4.prototype.getYaw = function( relativeRight ) {
-		//if( !relativeRight ) relativeRight = THREE.Vector3Right;
-		return Math.asin( this.forward.dot( relativeRight ) );
-	};
-	THREE.Matrix4.prototype.__defineGetter__( "roll", function(){
-		return this.getRoll( Vector3Up );
-	} );
-	THREE.Matrix4.prototype.__defineGetter__( "pitch", function(){
-		return this.getPitch( Vector3Forward );
-	} );
-	THREE.Matrix4.prototype.__defineGetter__( "yaw", function(){
-		return this.getYaw( Vector3Right );
-	} );
-//}
