49f47998effe7719773addeb75ca08085aac3bbc
d3x0r <d3x0r@no-reply.github.com>
2021-12-05 08:57:16 -0800

    Add radial lines for a source viewer of geodesics

--- math/mathSphereDecay.js
+++ math/mathSphereDecay.js
@@ -57,7 +57,7 @@ function readValues()  {
 	values.A = (Number(myForm.sliderA.value)/20.0);
 	values.Amax = (Number(myForm.sliderAmax.value)/20.0);
 	values.B = (Number(myForm.sliderB.value)/20.0);
-	values.B *= 1.5;
+	values.B *= 5;
 	values.C = (Number(myForm.sliderC.value)/10.0)-5;
 	values.D = (Number(myForm.sliderD.value)/10.0)-5;
 	myForm.sliderValA.textContent = values.A;
@@ -83,19 +83,28 @@ const _3to1 = (x,y,z)=> Math.sqrt(x*x+y*y+z*z);
 const _4to1 = (x,y,z,w)=> Math.sqrt(x*x+y*y+z*z+w*w);
 const _5to1 = (x,y,z,w,q)=> Math.sqrt(x*x+y*y+z*z+w*w+q*q);
 
-const Q_0 = _4to1;  // from real to converted (squash)
+// this adds a W component to a previously only x/y/z component scaled.
+const Q_0 = (x,y,z,w) => Math.sqrt(x*x+y*y+z*z+w*w);  // from real to converted (squash)
 const Q_i = (x,y,z,Q) => Math.sqrt( x*x+y*y+z*z-Q*Q );  // from converted to real (unsquash)
 const dQ_0 = (l,x,y,z,Q) => l/_4to1(x,y,z,Q);  // from real to converted (squash)
-const A_0 = (l,x,y,z,Q) => l/Math.sqrt(x*x+y*y+z*z) * Q_0(x,y,z,Q)
+
+// this normalizes a value from x/y/z -> x/y/z/w
+const A_0 = (l,x,y,z,w) => l/Math.sqrt(x*x+y*y+z*z) * Q_0(x,y,z,w)
 
 
+// this adds a Q component to a preivously only x/y/z/Q comonent
 const M_0 = _5to1;
 const M_00 = M_0;
 const M_ii = (x,y,z,w,q) => Math.sqrt( x*x+y*y+z*z-w*w-q*q );  // from converted to real (unsquash)
 const M_0i = (x,y,z,w,q) => Math.sqrt( x*x+y*y+z*z+w*w-q*q );  // from converted to real (unsquash)
 const M_i0 = (x,y,z,w,q) => Math.sqrt( x*x+y*y+z*z-w*w+q*q );  // from converted to real (unsquash)
 
+const dM_0 = (l,x,y,z,w,q) => (q)*(l/_4to1(x,y,z,w));  // from converted to real (unsquash)
+
+
 const AB_0 = (l,x,y,z,w,q) => l/_3to1(x,y,0) * M_0( x,y,0,q );
+
+// this normalizes a value from ( ( x/y/z -> x/y/z/w ) -> x/y/z/q )
 const B_0 = (l,x,y,z,w,q) => A_0(l,x,y,z,w)/_4to1(x,y,z,w) * M_0( x,y,z,w,q );
 
 // sequential space would enumerate in a sort of alternating sign, alternating reiprocal
@@ -122,9 +131,21 @@ const B_0 = (l,x,y,z,w,q) => A_0(l,x,y,z,w)/_4to1(x,y,z,w) * M_0( x,y,z,w,q );
 
 // if the underlaying spin in great enough, then the thing may be invisible, but still have a force.  (darkish matter... dense enough to displace any light wave around it without interacting
 
-// the inner
+// the inner expressions end up visible on the outside...
+// the layers aren't quite nesting like one would like.
 
+let mouseX=0, mouseY=0;
 
+canvas.addEventListener( "mousemove", (e)=>{
+	const rect = canvas.getBoundingClientRect();
+	const w = rect.right-rect.left;//window.innerWidth;
+	const h = rect.bottom-rect.top;//window.innerHeight;
+	const x = (((e.clientX-rect.left)-(w/2.0))/w) * 10;
+	const y = -(((e.clientY-rect.top)-(h/2.0))/h) * 10;
+	mouseX = x;
+	mouseY = y;
+	drawsomething();
+} );
 
 
 function drawsomething() {
@@ -176,7 +197,10 @@ function drawsomething() {
 
 	                   
 	for( let r = 0.01; r < 8; r+=0.5 ) {
+		// circles have no elongation to have to scale,it only needs the radius scaled.
 		const Gr = B_0(r,r,0,values.B,values.A, values.Amax );
+		// draw a circle with an aprox number of dots... could be smarter (or draw segments)
+		// the density could shade the pen also.
 		for( let t=0; t < Math.PI*2; t+= Math.PI*2/((r+1+values.A)*500) ) {
 			const x = Gr*Math.cos(t);
 			const y = Gr*Math.sin(t);
@@ -184,11 +208,19 @@ function drawsomething() {
 		}
 	}
 
+	//field is 2xg or 2g or g^2 for 2m?
+
+	
+
 	for( let r = -8.99; r < 8; r+=0.5 ) {
 		for( let t=-18.99; t < 18; t+= 5/1000 ) {
+
+			// these two draw the X/Y grid lines.
+			{
 				const Ax = B_0(t,t,r,values.B,values.A, values.Amax );
 				const Ay = B_0(r,t,r,values.B,values.A, values.Amax );
 				plot(Ax,Ay,pens[1] );
+			}
 			{
 // which is why this is parametarized across x,y,z,T axis... T isn't even a factor in this though
 				const Ax = B_0(r,t,r,values.B,values.A, values.Amax );
@@ -202,10 +234,11 @@ function drawsomething() {
 // this is change in virtual Y by time... 
 				{
 // which is why this is parametarized across x,y,z,T axis... T isn't even a factor in this though
-					const Ax = A_0(t,t,r,values.B,values.A, values.Amax )-t;
-					const Ay = A_0(r,t,r,values.B,values.A, values.Amax )-r;
-					const Bx = A_0(Ax,Ax,Ay,0, values.Amax );
-					const By = A_0(Ay,Ax,Ay,0, values.Amax );
+					const Ax = A_0(t,t,r,values.B,values.A)-t;
+					const Ay = A_0(r,t,r,values.B,values.A)-r;
+					const Az = A_0(values.B,t,r,values.B,values.A)-values.B;
+					const Bx = A_0(Ax,Ax,Ay,Az, values.Amax );
+					const By = A_0(Ay,Ax,Ay,Az, values.Amax );
 					//const Bx = Math.sign(Ax)*Ax*Ax/Math.sqrt(Ax*Ax+Ay*Ay);
 					//const By = Math.sign(Ay)*Ay*Ay/Math.sqrt(Ax*Ax+Ay*Ay);
 					plot(Bx,By,pens[4] );
@@ -213,10 +246,11 @@ function drawsomething() {
 				}
 				{
 // which is why this is parametarized across x,y,z,T axis... T isn't even a factor in this though
-					const Ax = A_0(r,t,r,values.B,values.A, values.Amax )-r;
-					const Ay = A_0(t,t,r,values.B,values.A, values.Amax )-t;
-					const Bx = A_0(Ax,Ax,Ay,0, values.Amax );
-					const By = A_0(Ay,Ax,Ay,0, values.Amax );
+					const Ax = A_0(r,t,r,values.B,values.A)-r;
+					const Ay = A_0(t,t,r,values.B,values.A)-t;
+					const Az = A_0(values.B,t,r,values.B,values.A)-values.B;
+					const Bx = A_0(Ax,Ax,Ay,Az, values.Amax );
+					const By = A_0(Ay,Ax,Ay,Az, values.Amax );
 					//const Bx = Math.sign(Ax)*Ax*Ax/Math.sqrt(Ax*Ax+Ay*Ay);
 					//const By = Math.sign(Ay)*Ay*Ay/Math.sqrt(Ax*Ax+Ay*Ay);
 					plot(Bx,By,pens[5] );
@@ -229,16 +263,16 @@ if(1) {
 // which is why this is parametarized across x,y,z,T axis... T isn't even a factor in this though
 			//const Ax = dQ_0(_3to1(t,r,values.B),t,r,values.B,values.Amax );
 			//const Ay = dQ_0(_3to1(t,r,values.B),t,r,values.B,values.Amax );
-			const Ax = dQ_0(r,t,r,_2to1(values.B,values.A),values.Amax )*values.Amax;
-			const Ay = dQ_0(t,t,r,_2to1(values.B,values.A),values.Amax )*values.Amax;
+			const Ax = dM_0(r,t,r,values.B,values.A,values.Amax );
+			const Ay = dM_0(t,t,r,values.B,values.A,values.Amax );
 			//plot(t,Ay,pens[3] );
 			//plot(t,Ax,pens[4] );
 			plot(Ax,Ay,pens[5] );
 			}
 			{
 // which is why this is parametarized across x,y,z,T axis... T isn't even a factor in this though
-			const Ax = dQ_0(t,t,r,values.B,values.Amax )*values.Amax;
-			const Ay = dQ_0(r,t,r,values.B,values.Amax )*values.Amax;
+			const Ax = dM_0(t,t,r,values.B,values.A,values.Amax );
+			const Ay = dM_0(r,t,r,values.B,values.A,values.Amax );
 			//plot(t,Ay,pens[3] );
 			//plot(t,Ax,pens[4] );
 			plot(Ax,Ay,pens[4] );
@@ -247,7 +281,7 @@ if(1) {
 		}
 
 	}
-	if(1)
+	if(0)
 	for( let r = -799; r < 800; r+=100 ) {
 		for( let t=-1899; t < 1800; t+= 1000/1000 ) {
 			if(1)
@@ -304,7 +338,19 @@ if(0)
 	}
 
 
-	
+	for( let t = 0; t < 360; t+= 10 ) {
+		let slopex = Math.cos( t/180*Math.PI );
+		let slopey = Math.sin( t/180*Math.PI );
+		for( let t=0; t < 10; t+= 10/1000 ) {
+			const mx = mouseX + slopex * t;
+			const my = mouseY + slopey * t;
+				const Ax = B_0(mx,mx,my,values.B,values.A, values.Amax );
+				const Ay = B_0(my,mx,my,values.B,values.A, values.Amax );
+			//if( Math.sqrt(Ax*Ax+Ay*Ay)< (values.A+values.Amax+1) ) continue;
+			if( Math.sqrt(mx*mx+my*my)< (1) ) break;
+			plot(Ax,Ay, pens[0] );
+		}
+	}
 
 	ctx.putImageData(_output, 0,0);
 
