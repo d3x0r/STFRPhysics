83c6f720ca6e189227c4d735542c72ec4c569652
d3x0r <d3x0r@no-reply.github.com>
2023-12-27 03:22:27 -0800

    Update IO interface with comments, and protocol

--- hardware/fpga/Counter.v
+++ hardware/fpga/Counter.v
@@ -21,24 +21,42 @@
 module COUNTER #(
 pWIDTH=40
 ) (
-input               iLatch1,
-input               iLatch2,
-input               iResetLatch1,
-input               iResetLatch2,
-output [pWIDTH-1:0] oCOUNTER,
-output [pWIDTH-1:0] oCOUNTER2,
-output  oRdyCOUNTER,
-output  oRdyCOUNTER2
+input               iLatch1,      // signal event that latches the counter to register 1
+input               iLatch2,      // signal event that latches the counter to register 2
+input               iResetLatch1, // sent after the value in register 1 is read from (or sent to) the USB
+input               iResetLatch2, // sent after the value in register 2 is read from (or sent to) the USB
+output [pWIDTH-1:0] oCOUNTER,  // register 1 with latched counter value
+output [pWIDTH-1:0] oCOUNTER2, // register 2 with latched counter value
+output  oRdyCOUNTER,           // signals that data has been latched in register 1
+output  oRdyCOUNTER2           // signals that data has been latched in register 2
 );
-reg [pWIDTH-1:0] rCOUNTER = 0;
-reg [pWIDTH-1:0] rLatch1 = 0;
-reg [pWIDTH-1:0] rLatch2 = 0;
-reg iCLK = 0;
-reg latchLock1 = 0;
-reg latchLock2 = 0;
-reg rstLatchLock1 = 0;
-reg rstLatchLock2 = 0;
+reg [pWIDTH-1:0] rCOUNTER = 0; // live counter, always increments
+reg [pWIDTH-1:0] rLatch1 = 0;  // latched counter value 1
+reg [pWIDTH-1:0] rLatch2 = 0;  // latched counter value 2
+reg iCLK = 0;  // the flip-flopped clock gate
+reg latchLock1 = 0;     // iLatch1 was set, and the value is copied to rLatch1; prevents update to register 1 until reset
+reg latchLock2 = 0;     // iLatch2 was signaled, the value is copied to rLatch2; prevents update to reigster 2 until reset
+reg rstLatchLock1 = 0;  // pending reset signal, iResetLatch1 was signaled, but iLatch1 is still active; 
+			// wait until iLatch1 is not in a signaled state
+reg rstLatchLock2 = 0;  // pending reset signal, iResetLatch2 was signaled, but iLatch2 is still active; 
+			// wait until iLatch2 is not in a signaled state
 
+// if iResetLatch1 or iResetlatch2 is signaled, and iLatch1 or iLatch2 respectively is not set, then
+// latchLock1 or latchLock2 is immediately cleared.
+
+// if iResetLatch* is signaled and iLatch* is still set, then set rstLatchLock*, then when latchLock* is reset
+// if rstLatchLock* is set, reset latckLock*.
+
+// protocol should resemble something like
+//   Host receives 00 + 5 byte counter value
+//   Host sends 0 to clear register 1 latch
+//   host receives 01 + 5 byte counter value
+//   Host sends 1 to clear register 2 latch
+// or from the other side:
+//   slave sends 00 + 5 byte counter value when register 1 is latched
+//   slave receives 00 and gneerates iResetLatch1
+//   slave sends 01 + 5 byte counter value when register 2 is latched
+//   slave receives 01 and gneerates iResetLatch2
 
 initial forever // Start at time 0 and repeat the begin/end forever
   begin
