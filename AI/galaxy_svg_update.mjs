2b83df52a8f586a58a802a640fb2968cb1981e67
d3x0r <d3x0r@no-reply.github.com>
2026-05-13 21:34:14 -0700

    Use/integrate galaxy SVG generator

---
+++ AI/galaxy_svg_update.mjs
@@ -0,0 +1,18 @@
+import { generateGalaxySvg } from './galaxy_svg.mjs';
+import { GALAXY_PRESETS } from './galaxy_presets.mjs';
+import { ROTMOD_GALAXIES } from './rotmod_ltg_data.mjs';
+
+const data = { presets: GALAXY_PRESETS, rotmod: ROTMOD_GALAXIES };
+import {sack} from "sack.vfs";
+const disk = sack.Volume();
+
+let currentSvg = '';
+let currentName = '';
+
+console.log( "data.presets" );
+for( let name of Object.keys( data.presets ) ) {
+	const g = data.presets[name];
+	const s = generateGalaxySvg( name, data );
+        disk.write( "svg/"+name+".svg", s );
+}
+
