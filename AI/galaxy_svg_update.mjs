0fdbab7e0e1114866afe9c97496325431e03c36f
d3x0r <d3x0r@no-reply.github.com>
2026-05-31 12:32:20 -0700

    Update to have some tests commented out

--- AI/galaxy_svg_update.mjs
+++ AI/galaxy_svg_update.mjs
@@ -1,5 +1,6 @@
 import { generateGalaxySvg } from './galaxy_svg_with_photometric_profile.mjs';
-//import { generateGalaxySvg } from './galaxy_svg_with_mond_download.mjs';
+//import { generateGalaxySvg as g2 } from './galaxy_svg_with_mond_download.mjs';
+//import { generateGalaxySvg as g2 } from './galaxy_svg.mjs';
 import { GALAXY_PRESETS } from './galaxy_presets.mjs';
 import { ROTMOD_GALAXIES } from './rotmod_ltg_data.mjs';
 
@@ -15,5 +16,7 @@ for( let name of Object.keys( data.presets ) ) {
 	const g = data.presets[name];
 	const s = generateGalaxySvg( name, data );
         disk.write( "svg/"+name+".svg", s );
+//	const s2 = g2( name, data );
+//        disk.write( "svg/"+name+"2.svg", s2 );
 }
 
